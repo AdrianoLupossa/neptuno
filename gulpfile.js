1cfdd7c6df72fd71f4a275b8858fbefdb8dc856a
adrianolupossa <adrianolupossa@gmail.com>
2001-01-02 06:57:04 +0100

    add taskrunner - gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require("gulp"),
+	stylus = require("gulp-stylus"),
+	minifycss = require("gulp-minify-css"),
+	autoprefixer = require("gulp-autoprefixer"),
+	uglify = require("gulp-uglify"),
+	browserSync = require("browser-sync"),
+	plumber = require("gulp-plumber");
+	;
+
+// FILE SOURCES AND FILE OUTPUTS
+var target = {
+	stylus_src: "neptuno.styl",
+	cssOutput: "dist/css/"
+};
+
+/* STYLES TASKS */
+gulp.task("styles", function(){
+	gulp.src(target.stylus_src)
+	.pipe(plumber())
+	.pipe(stylus("compressed"))
+	.pipe(autoprefixer(
+		'last 2 version',
+		'> 1%',
+		'ie 8',
+		'ie 9',
+		'ios 6',
+		'android 4'
+	))
+	.pipe(minifycss())
+	.pipe(gulp.dest(target.cssOutput));
+});
+
+/* LIVERELOAD WITH BROWSER-SYNC*/
+gulp.task("browserSync", function(){
+	browserSync.init(["dist/**/*", "index.html"], {
+		server: {
+			host: "localhost"
+		},
+		open: true
+	});
+});
+
+/* WATCH TASKS */
+gulp.task("watch-styles", function(){
+	gulp.watch(["styles/**/*.styl"], ["styles"]);
+});
+
+gulp.task("default", ["styles", "browserSync", "watch-styles"]);
